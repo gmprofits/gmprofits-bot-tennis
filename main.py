10110ab59be8f3174a4f56e5c0deda98842435d6
gmprofits <gmprofits.ia@gmail.com>
2025-08-05 23:28:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,10 @@ notificati = set()
 
 # Log
 logging.basicConfig(level=logging.INFO)
-headers = {"User-Agent": "Mozilla/5.0"}
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36"
+}
+
 
 @app.route('/')
 def home():
